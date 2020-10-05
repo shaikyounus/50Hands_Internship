1a3019c04482534a3ec9b7476d29df7393447cca
SHAIK MOHAMMAD YOUNUS <37431701+shaikyounus@users.noreply.github.com>
2020-10-05 10:01:20 +0530

    Add files via upload

---
+++ USA-TB-DB-UPLOAD.py
@@ -0,0 +1,84 @@
+import pymysql.cursors
+f=open(r"USA-TB-States.csv","r")
+fString=f.read()
+fList=[]
+
+
+
+for line in fString.split('\n'):
+    fList.append(line.split(','))
+
+# Connect to the database
+#connection = pymysql.connect(host="213.190.6.106",
+                             #user="u831388458_covid19",
+                             #password="Password@123",
+                             #db="u831388458_covid19stats")
+
+connection = pymysql.connect(host="localhost",
+                             user="root",
+                             password="",
+                             db="sampletestdb")
+
+
+rows=""
+try:
+
+    
+    with connection.cursor() as cursor:
+        cursor.execute("""DROP TABLE IF EXISTS USA_Comorbidity_TB_Province_level""")
+        cursor.execute("""CREATE TABLE USA_Comorbidity_TB_Province_level
+(
+
+ReportingArea text(255),
+cases_2018 text(200),
+cases_2017 text(200),
+caserates_2018 text(200),
+caserates_2017 text(200),
+rank_2018 text(200),
+rank_2017 text(200)
+)
+""")
+        
+
+    
+        
+        for i in range(3,len(fList)-16):
+            #fList[i][1]=db.escape_string(fList[i][1])
+            
+            rows+="('{}','{}','{}','{}','{}','{}','{}')".format(str(fList[i][1]),str(fList[i][2]),str(fList[i][3]),str(fList[i][4]),str(fList[i][5]),str(fList[i][6]),str(fList[i][7]))
+                                       
+            if i!=len(fList)-17:
+                
+                rows+=','
+                
+                                       
+                                       
+                            
+                                           
+                
+                                    
+
+
+
+
+        
+        queryInsert="""
+
+
+INSERT INTO USA_Comorbidity_TB_Province_level VALUES """+rows
+        cursor.execute(queryInsert)
+        
+
+        connection.commit()
+    
+    
+ 
+        
+finally:
+    connection.close()
+
+
+
+
+
+
